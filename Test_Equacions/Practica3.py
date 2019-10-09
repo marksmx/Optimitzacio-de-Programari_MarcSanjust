adaad3b7437cb74a72b80fdade6be6c7ed72d388
marksmx <32665386+marksmx@users.noreply.github.com>
2019-10-09 19:31:17 +0200

    Update Practica3.py

--- Test_Equacions/Practica3.py
+++ Test_Equacions/Practica3.py
@@ -1,25 +1,35 @@
 import unittest
 from Practica31 import Equacio
 
-class TestEq(unittest.TestCase):
-    def test_eq(self):
-        #Test 0 - Registre d'errors
-        eq = Equacio("20x * 30 = ERROR")
-        eq.calcula()
-        self.assertEqual(eq.b, '*')
-        self.assertEqual(eq.e, 'Error')
-        #Test 1 - Numeros Positius
-        self.assertEqual(Equacio("20x + 30 = 70").calcula(),2.0)
-        #Test 2 - Numeros Negatius
-        self.assertEqual(Equacio("200x - 50 = -78").calcula(),-0.14)
-        #Test 3 - Numeros Positius i Negatius
-        self.assertEqual(Equacio("5x + 8 = -6").calcula(),-2.8)
-        #Test 4 - Valor 0
-        self.assertEqual(Equacio("4x - 28 = 0").calcula(),7.0)
-        #Test 5 - Lletra en comptes de numero
-        self.assertEqual(Equacio("4x - 28 = d").calcula(),7.0)
-        #Test 6 - Operador no valid
-        self.assertEqual(Equacio("4x * 28 = 0").calcula(),7.0)
 
-if __name__ == "__main__":
-    unittest.main();
+class TestEquacio(unittest.TestCase):
+
+    def test_positiu(self):
+        eq = Equacio("2x + 3 = 7")
+        self.assertEqual(eq.calcula(),2)
+
+    def testincorrecte(self):
+        eq = Equacio("2x / 3 = 7")
+        self.assertEqual(eq.calcula(),"Operador no valid: "+eq.b)
+        self.assertIsInstance(eq.b, basestring)
+
+    def test_negatiu(self):
+        eq = Equacio("2x - 3 = 7")
+        self.assertEqual(eq.calcula(),5)
+
+    def test_float(self):
+        eq = Equacio("2.3x - 8.4 = 9.8")
+        self.assertEqual(eq.calcula(),7.913043478260872)
+
+    def test_negatiu(self):
+        eq = Equacio("2x - p = 7")
+        self.assertEqual(eq.calcula(),"l'equacio conte caracter no calculables: "+eq.eq)
+
+    def test_fromat_erroni(self):
+        eq = Equacio("3 - 2x = 7")
+        self.assertEqual(eq.calcula(),"l'equacio no segueix el format: ax + b = c")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
