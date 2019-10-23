6b93c4946cee669a96a82f63d2500eae62b1474b
marksmx <32665386+marksmx@users.noreply.github.com>
2019-10-23 18:04:55 +0200

    Update BOTScript.py

--- Sanjust_Marc_Bot/BOTScript.py
+++ Sanjust_Marc_Bot/BOTScript.py
@@ -20,37 +20,46 @@ def test(message):
 def test(message):
 	lista=(random.randint(1,5))
 	switcher = {
-		1: "Me acaba de picar una serpiente!", # Cobra?  No, idiota, lo ha hecho gratis!
-		2: "Buenos dias. Busco trabajo.", # jardinero?  Dejar dinero? Si lo que busco es trabajo!
-		3: "Hola, soy paraguayo y quiero pedirle la mano de su hija para casarme con ella.", # Para que? Paraguayo.
-		4: "Buenos dias, queria una camiseta de un personaje inspirador.", # Ghandi? No, mediani.
-		5: "Sabes? Hoy me he comprado una paloma que cuesta diez mil euros." #Mensajera? No no, no te exagero
+		1: "Me acaba de picar una serpiente!",
+		2: "Buenos dias. Busco trabajo.",
+		3: "Hola, soy paraguayo y quiero pedirle la mano de su hija para casarme con ella.",
+		4: "Buenos dias, queria una camiseta de un personaje inspirador.",
+		5: "Sabes? Hoy me he comprado una paloma que cuesta diez mil euros."
 	}
 	bot.reply_to(message, switcher.get(lista,"Error"))
 
-	if lista == 1:
-		@bot.message_handler(commands=['Cobra'])
-		def test(message):
-			bot.reply_to(message, "No, idiota, lo ha hecho gratis!")
+	preg = False
 
-	if lista == 2:
-		@bot.message_handler(commands=['Jardinero'])
-		def test(message):
-			bot.reply_to(message, "Dejar dinero? Si lo que busco es trabajo!")
+	if preg == False:
+		if lista == 1:
+			@bot.message_handler(commands=['Cobra'])
+			def test(message):
+				bot.reply_to(message, "No, idiota, lo ha hecho gratis!")
+			preg = True
 
-	if lista == 3:
-		@bot.message_handler(commands=['Para'])
-		def test(message):
-			bot.reply_to(message, "Paraguayo")
+		if lista == 2:
+		   @bot.message_handler(commands=['Jardinero'])
+		   def test(message):
+			   bot.reply_to(message, "Dejar dinero? Si lo que busco es trabajo!")
+		   preg = True
 
-	if lista == 4:
-		@bot.message_handler(commands=['Ghandi'])
-		def test(message):
-			bot.reply_to(message, "No, mediani.")
+		   if lista == 3:
+			@bot.message_handler(commands=['Para'])
+			def test(message):
+			  bot.reply_to(message, "Paraguayo")
+  	  	  	preg = True
 
-	if lista == 5:
-		@bot.message_handler(commands=['Mensajera'])
-		def test(message):
-			bot.reply_to(message, "No no, no te exagero")
+		if lista == 4:
+			@bot.message_handler(commands=['Ghandi'])
+			def test(message):
+				bot.reply_to(message, "No, mediani.")
+			preg = True
+
+			if lista == 5:
+		 		@bot.message_handler(commands=['Mensajera'])
+		 		def test(message):
+			 		bot.reply_to(message, "No no, no te exagero")
+	     		preg = True
 
 bot.polling()
+
